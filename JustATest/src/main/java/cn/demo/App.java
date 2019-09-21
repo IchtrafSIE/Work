b118f804b3c652dfb20a91cb1ea6aceefe667f99
IchtrafSIE <87557189@qq.com>
2019-09-21 10:40:45 +0800

    Test

--- JustATest/src/main/java/cn/demo/App.java
+++ JustATest/src/main/java/cn/demo/App.java
@@ -6,8 +6,9 @@ package cn.demo;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+        System.out.println("not just a hello");
+
     }
 }
